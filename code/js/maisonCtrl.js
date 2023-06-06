6670115751c666592eaf9a3e95df1ef3fc2c7972
Brodardc <clara.brodard@outlook.fr>
2023-06-06 11:42:31 +0200

    Update maisonCtrl.js

--- code/js/maisonCtrl.js
+++ code/js/maisonCtrl.js
@@ -1,11 +1,12 @@
 /*
-  But :     
-  Auteur : prénom + nom
-  Date :   jj.mm.aaaa / V1.0
+  But :     controleur de la vue maison
+  Auteur : Brodard Clara
+  Date :   06.06.2023 / V1.0
 */
-class CompteCtrl {
+class maisonCtrl {
   constructor() {
     // Ajouter un écouteur par bouton. On est obligé de le faire là car l'objet n'est pas connu dans le html si on le fait directement dans l'html
+    // bouton maison et personnages
     $("#compte-valider").click(() => {
       this.validerCompte();
     });
