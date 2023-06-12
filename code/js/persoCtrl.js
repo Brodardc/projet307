49925fa46b5a4bccab67942ae54087a8332a836d
Brodardc <clara.brodard@outlook.fr>
2023-06-12 16:44:42 +0200

    Update persoCtrl.js

--- code/js/persoCtrl.js
+++ code/js/persoCtrl.js
@@ -7,11 +7,30 @@ class persoCtrl {
   constructor() {
     // Ajouter un écouteur par bouton. On est obligé de le faire là car l'objet n'est pas connu dans le html si on le fait directement dans l'html
     // bouton maison et personnages
-    $("#boutonMaison").click(() => {
+    $("#afficheMaison").click(() => {
       this.allerMaison();
     });
+    this.afficherPersonne();
   }
-  
+
+  afficherPersonne() {
+    console.log(httpApiP.jsonP);
+    let tab = httpApiP.jsonP;
+
+
+    tab.forEach(element => {
+      $("#afficherPero").append(
+        "<div class='affichePerso'>"+
+        "<p id='nom'>" + element.firstName + " " +element.lastName+ "</p>"+
+        "<img src='" + element.imageUrl+"'width='100%'/>"+
+        "<p>Titre : "+element.title+"</p>" +
+        "<p>Famille : "+element.family+"</p>"+
+        "<p>Nom complet : "+element.fullName+"</p>"+
+        "</div>");
+      console.log(element.firstName);
+    });
+  }
+
   allerMaison() {
     indexCtrl.loadMaison();
   }
