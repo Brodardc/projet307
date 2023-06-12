a445f5c709dd3baf202828712277f0d9254e54aa
Brodardc <clara.brodard@outlook.fr>
2023-06-12 16:44:40 +0200

    Update maisonCtrl.js

--- code/js/maisonCtrl.js
+++ code/js/maisonCtrl.js
@@ -8,38 +8,31 @@ class maisonCtrl {
     // Ajouter un écouteur par bouton. On est obligé de le faire là car l'objet n'est pas connu dans le html si on le fait directement dans l'html
     // bouton maison et personnages
     $("#affichePersonnages").click(() => {
-      this.loadPersonnages();
+      this.allerPersonnages();
     });
-
+    this.afficherMaison();
   }
+  afficherMaison() {
+    console.log(httpApiM.jsonM);
+    let tab = httpApiM.jsonM;
 
-  validerCompte() {
-    // récupère les 5 valeurs de l'interface graphique via JQuery
-    let identifiant = {
-      username: $("#username").val(),
-      password: $("#password").val(),
-      domaine: $("#domaine").val(),
-      mail: $("#mail").val(),
-      langue: $("input[type='radio'][name='langues']:checked").val(),
-    };
 
-    //service.login(identifiant, this.OKCompte);
-    http.login(identifiant, (data) => {
-      this.OKCompte(data);
+    tab.forEach(element => {
+      $("#afficherMaison").append(
+        "<div class='afficheMaison'>"+
+        "<p id='nomMaison'>" + element.name + "</p>"+
+        "<p>Region : "+element.region+"</p>" +
+        "<p>Armoiries : "+element.coatOfArms+"</p>"+
+        "<p>Armoiries : "+element.coatOfArms+"</p>"+
+        "<p>Citation : "+element.words+"</p>"+
+        "<p>Fondation : "+element.founded+"</p>"+
+        "</div>");
+      console.log(element.name);
     });
   }
+  
 
-
-  OKCompte(data) {
-    if (data.includes("KO")) {
-      alert("Vos identifications sont incorrectes !!!\nVeuillez réessayer...");
-    } else {
-      alert(`Enregistrement OK:  ${data}`);
-      indexCtrl.loadLogin();
-    }
-  }
-
-  loadPersonnages() {
+  allerPersonnages() {
     indexCtrl.loadPersonnages();
   }
 }
